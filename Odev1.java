6937a6e64ea7f18c5c552f4363f3b2c21a57d81d
Abdulkerim Kavanoz <akerimkavanoz@gmail.com>
2023-10-28 16:36:00 +0300

    Ödev 1
    Ödev 1

---
+++ Odev1.java
@@ -0,0 +1,146 @@
+package com.example.javadersleri.degiskenler;
+
+public class Odev1 {
+    public static void main(String[] args) {
+        String ilce = "Akçaabat";
+        System.out.println("İlçe : " +ilce);
+
+        String kita = "Asya Kıtası";
+        System.out.println("Kıta : " +kita);
+
+        String faks = "9902282141042";
+        System.out.println("Faks : " +faks);
+
+        int postaKodu = 61300;
+        System.out.println("Posta Kodu : " +postaKodu);
+
+        String instaAdresi = "@akerimkavanoz";
+        System.out.println("İnsta Adresi : " +instaAdresi);
+
+        String calistiginBolum = "Bilgisayar Mühendisi";
+        System.out.println("Çalıştığın Bölüm : " +calistiginBolum);
+
+        int urunMiktari = 5;
+        System.out.println("Ürün Miktarı : " +urunMiktari);
+
+        String musteriSoyadi = "Kavanoz";
+        System.out.println("Müşteri Soyadı : " +musteriSoyadi);
+
+        double odemeMiktari = 249.99;
+        System.out.println("Ödeme Miktarı : " +odemeMiktari +" TL");
+
+        String dogumTarihi = "01.01.2000";
+        System.out.println("Doğum Tarihi : " +dogumTarihi);
+
+        double borc = 857.5;
+        System.out.println("Borç : " +borc +" TL");
+
+        char medeniHal = 'E';
+        System.out.println("Medeni Hal : " +medeniHal);
+
+        String videoYorumu = "Çok faydalı bir video.";
+        System.out.println("Video Yorumu : " +videoYorumu);
+
+        String odemeSaati = "19:00";
+        System.out.println("Ödeme Saati : " +odemeSaati);
+
+        double eftMiktari = 1049.75;
+        System.out.println("Eft Miktarı : " +eftMiktari +" TL");
+
+        int satilanMiktar = 47;
+        System.out.println("Satılan Miktar : " +satilanMiktar);
+
+        String telefonModeli = "Samsung Galaxy S23";
+        System.out.println("Telefon Modeli : " +telefonModeli);
+
+        String dergiAdi = "Arka Kapı";
+        System.out.println("Dergi Adı : " +dergiAdi);
+
+        String basimTarihi = "12.04.2012";
+        System.out.println("Basım Tarihi : " +basimTarihi);
+
+        int zamMiktari = 3000;
+        System.out.println("Zam Miktarı : " +zamMiktari +" TL");
+
+        int daireSayisi = 20;
+        System.out.println("Daire Sayısı : " +daireSayisi);
+
+        String enlem = "15°K";
+        System.out.println("Enlem : " +enlem);
+
+        String boylam = "30°D";
+        System.out.println("Boylam : " +boylam);
+
+        String yemekAdi = "Makarna";
+        System.out.println("Yemek Adı: " +yemekAdi);
+
+        double urunFiyati = 15.0;
+        System.out.println("Ürün Fiyatı : " +urunFiyati +" TL");
+
+        String sirket = "Techcareer";
+        System.out.println("Şirket : " +sirket);
+
+        String videoAdi = "Java Android Programlama Ders 1";
+        System.out.println("Video Adı : " +videoAdi);
+
+        double muzikSuresi = 4.5;
+        System.out.println("Müzik Süresi : " +muzikSuresi + " dakika");
+
+        int mekanPuani = 8;
+        System.out.println("Mekan Puanı : " +mekanPuani);
+
+        String dosyaAdi = "Ödev1.java";
+        System.out.println("Dosya Adı : " +dosyaAdi);
+
+        String resimFormati = ".JPEG";
+        System.out.println("Resim Formatı : " +resimFormati);
+
+        String renk = "Mavi";
+        System.out.println("Renk : " +renk);
+
+        String renkKodu = "#0000ff";
+        System.out.println("Renk Kodu : " +renkKodu);
+
+        String bilgisayarModeli = "Lenovo Ideapad";
+        System.out.println("Bilgisayar Modeli : " +bilgisayarModeli);
+
+        double ekranBoyutu = 15.6;
+        System.out.println("Ekran Boyutu : " +ekranBoyutu +" inç");
+
+        int kullanimSuresi = 2;
+        System.out.println("Kullanım Süresi : " +kullanimSuresi);
+
+        double basinc = 1013.25;
+        System.out.println("Basınç : " +basinc +" mb");
+
+        String etkinlikGunu = "29 Ekim Pazar";
+        System.out.println("Etkinlik Günü : " +etkinlikGunu);
+
+        String odemeGunu = "30 Ekim Pazartesi";
+        System.out.println("Ödeme Günü : " +odemeGunu);
+
+        String yolculukCikisTarihi = "18.12.2020";
+        System.out.println("Yolculuk Çıkış Tarihi : " +yolculukCikisTarihi);
+
+        String mahalleAdi = "Yayla";
+        System.out.println("Mahalle Adı : " +mahalleAdi);
+
+        String otobusHatti = "103 Karşıyaka";
+        System.out.println("Otobüs Hattı : " +otobusHatti);
+
+        int kullanilanDakika = 40;
+        System.out.println("Kullanılan Dakika : " +kullanilanDakika);
+
+        String kargoTakipNo = "1234 5678 9100";
+        System.out.println("Kargo Takip No : " +kargoTakipNo);
+
+        int kuponSuresi = 15;
+        System.out.println("Kupon Süresi : " +kuponSuresi +" gün");
+
+        String kuponKodu = "ILK100";
+        System.out.println("Kupon Kodu : " +kuponKodu);
+
+        String faturaTarihi = "20.08.2023";
+        System.out.println("Fatura Tarihi : " +faturaTarihi);
+    }
+}
