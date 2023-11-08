79a4e1bb4c113e08b9c9a0f0107749bf31c8e1c7
Abdulkerim Kavanoz <akerimkavanoz@gmail.com>
2023-11-08 18:16:07 +0300

    Ödev 4

---
+++ Odev4/app/src/main/java/com/example/odev4/SayfaYFragment.java
@@ -0,0 +1,25 @@
+package com.example.odev4;
+
+import android.os.Bundle;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.odev4.databinding.ActivityMainBinding;
+
+
+public class SayfaYFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_sayfa_y, container, false);
+    }
+
+}
